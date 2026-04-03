917983573cd3493337566cb5b6b0c2079a429648
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-04 01:41:00 +0530

    update StudentController

--- src/main/java/com/nethmal/WebSocket_project/Controller/StudentController.java
+++ src/main/java/com/nethmal/WebSocket_project/Controller/StudentController.java
@@ -4,10 +4,7 @@ import com.nethmal.WebSocket_project.Dto.StudentRequest;
 import com.nethmal.WebSocket_project.Dto.StudentResponse;
 import com.nethmal.WebSocket_project.Service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,7 @@ public class StudentController {
     private final StudentService studentService;
 
     @PostMapping
-    public StudentResponse addStudent(StudentRequest request) {
+    public StudentResponse addStudent(@RequestBody StudentRequest request) {
         return studentService.saveStudent(request);
     }
 
