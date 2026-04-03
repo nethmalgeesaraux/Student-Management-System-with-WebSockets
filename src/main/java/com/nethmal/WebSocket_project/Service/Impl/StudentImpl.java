a88cb9425cb99bd1f298ad25eca950d0e7063317
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-04 01:19:15 +0530

    update Service

--- src/main/java/com/nethmal/WebSocket_project/Service/Impl/StudentImpl.java
+++ src/main/java/com/nethmal/WebSocket_project/Service/Impl/StudentImpl.java
@@ -1,5 +1,7 @@
 package com.nethmal.WebSocket_project.Service.Impl;
 
+import com.nethmal.WebSocket_project.Dto.StudentRequest;
+import com.nethmal.WebSocket_project.Dto.StudentResponse;
 import com.nethmal.WebSocket_project.Entity.StudentEntity;
 import com.nethmal.WebSocket_project.Repository.StudentRepository;
 import com.nethmal.WebSocket_project.Service.StudentService;
@@ -15,12 +17,26 @@ public class StudentImpl implements StudentService {
     private final StudentRepository studentRepository;
 
     @Override
-    public StudentEntity saveStudent(StudentEntity student) {
-        return studentRepository.save(student);
+    public StudentResponse saveStudent(StudentRequest studentRequest) {
+        StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setName(studentRequest.getName());
+        studentEntity.setEmail(studentRequest.getEmail());
+
+        StudentEntity save = studentRepository.save(studentEntity);
+
+        return new StudentResponse(
+                save.getId(),
+                save.getName(),
+                save.getEmail());
     }
 
     @Override
-    public List<StudentEntity> getAllStudents() {
-        return studentRepository.findAll();
+    public List<StudentResponse> getAllStudents() {
+        return studentRepository.findAll().stream()
+                .map(studentEntity -> new StudentResponse(
+                        studentEntity.getId(),
+                        studentEntity.getName(),
+                        studentEntity.getEmail()
+                )).toList();
     }
 }
--- src/main/java/com/nethmal/WebSocket_project/Service/StudentService.java
+++ src/main/java/com/nethmal/WebSocket_project/Service/StudentService.java
@@ -1,11 +1,13 @@
 package com.nethmal.WebSocket_project.Service;
 
+import com.nethmal.WebSocket_project.Dto.StudentRequest;
+import com.nethmal.WebSocket_project.Dto.StudentResponse;
 import com.nethmal.WebSocket_project.Entity.StudentEntity;
 
 import java.util.List;
 
 public interface StudentService {
 
-    StudentEntity saveStudent(StudentEntity student);
-    List<StudentEntity> getAllStudents();
+   StudentResponse saveStudent(StudentRequest studentRequest);
+    List<StudentResponse> getAllStudents();
 }
